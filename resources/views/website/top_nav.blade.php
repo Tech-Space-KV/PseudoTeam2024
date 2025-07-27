a3039f3717d5cfe268c5873d5dc1cbf5c01e9ff0
Sanskar shamra <sanskarsharma0119@gmail.com>
2025-07-28 01:11:25 +0530

    Changes

--- resources/views/website/top_nav.blade.php
+++ resources/views/website/top_nav.blade.php
@@ -1,18 +1,18 @@
-
-<nav class="navbar navbar-expand-lg navhide" style="background-color: rgba(21, 21, 21, 0.812); backdrop-filter: blur(30px);">
+<nav class="navbar navbar-expand-lg navhide"
+  style="background-color: rgba(21, 21, 21, 0.812); backdrop-filter: blur(30px);">
   <div class="container-fluid px-4">
     <!-- Logo -->
     {{-- <a class="navbar-brand" href="#">
       <img src="https://upload.wikimedia.org/wikipedia/commons/3/3e/OYO_Rooms_logo.svg" alt="OYO Logo" />
     </a> --}}
-{{-- 
+    {{--
     <img src="{{asset ('images/logopt.png')}}" class="me-2" style="width: 120px;" /> --}}
 
     <!-- Toggler for Mobile -->
     {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button> --}}
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button> --}}
 
     <!-- Menu Items -->
     <div class="collapse navbar-collapse" id="oyo">
@@ -28,7 +28,7 @@
           <div class="title">PseduoTeam for Business</div>
           <div class="subtitle">Trusted by 500+ Corporates</div>
         </li>
-         <div class="nav-divider d-none d-lg-block"></div>
+        <div class="nav-divider d-none d-lg-block"></div>
         <li class="nav-item text-center">
           <div class="nav-icon"><i class="fa-solid fa-map"></i></div>
           <div class="title">Location Agnostic</div>
@@ -80,54 +80,66 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-dark px-2 sticky-top px-4"
-        style="background-color: rgba(21, 21, 21, 0.812); backdrop-filter: blur(30px);">
-        <div class="container-fluid">
-            <a href="{{ route('home') }}"><img src="{{asset ('images/logopt.png')}}" class="me-2" style="width: 120px;" /></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link nav-text" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-text" href="#services">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-text" href="#about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-text" href="#contactus">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link nav-text" href="#contactus">Our Products</a>
-                    </li>
-                    {{-- <a href="" style="text-decoration: none"><li class="nav-item">
-                        <form class="d-flex nav-link nav-text" role="search">
-                            <div class="input-group">
-  <input type="text" class="form-control form-control-sm" placeholder="Search a service here" aria-label="Search a service here" aria-describedby="button-addon2">
-  <button class="btn btn-sm btn-outline-secondary" type="button" id="button-addon2">&#128269;</button>
-</div>
-                        </form>
-                    </li></a> --}}
+  style="background-color: rgba(21, 21, 21, 0.812); backdrop-filter: blur(30px);">
+  <div class="container-fluid">
+    <a href="{{ route('home') }}"><img src="{{asset('images/logopt.png')}}" class="me-2" style="width: 120px;" /></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0" id="menubar">
+        <li class="nav-item">
+          <a class="nav-link nav-text" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-text" href="#services">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-text" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-text" href="#contactus">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link nav-text" href="#contactus">Our Products</a>
+        </li>
+        {{-- <a href="" style="text-decoration: none">
+          <li class="nav-item">
+            <form class="d-flex nav-link nav-text" role="search">
+              <div class="input-group">
+                <input type="text" class="form-control form-control-sm" placeholder="Search a service here"
+                  aria-label="Search a service here" aria-describedby="button-addon2">
+                <button class="btn btn-sm btn-outline-secondary" type="button" id="button-addon2">&#128269;</button>
+              </div>
+            </form>
+          </li>
+        </a> --}}
 
-                </ul>
+      </ul>
 
-                <span class="navbar-text">
-                    <a class="nav-link nav-text px-4 glow-text" href="{{ route('ask_for_quote') }}" {{--data-bs-toggle="modal" data-bs-target="#exampleModal"--}}>Get Critical Spares & IT Services</a>
-                </span>
-                <span class="navbar-text">
-                    <a class="btn btn-outline-light px-4" onmouseover="this.style.color='black';"
-                        onmouseout="this.style.color='white';" href="{{ url('/authentication/customer/sign-in') }}">Manage Project</a>
-                </span>
-            </div>
-        </div>
-    </nav>
+      <span class="navbar-text">
+        <a class="nav-link nav-text px-4 glow-text" href="{{ route('ask_for_quote') }}" {{--data-bs-toggle="modal"
+          data-bs-target="#exampleModal" --}}>Get Critical Spares & IT Services</a>
+      </span>
+      <span class="navbar-text">
+        <a class="btn btn-outline-light px-4" onmouseover="this.style.color='black';"
+          onmouseout="this.style.color='white';" href="{{ url('/authentication/customer/sign-in') }}">Manage Project</a>
+      </span>
+    </div>
+  </div>
+</nav>
 
-    <script>
-      window.onload = function() {
-        document.getElementById('menubar').style.display = 'none';
-      };
-    </script>
+<script>
+  window.onload = function () {
+    if (window.location.hostname != "https://pseudoteam.com/PseudoTeam2024/public/") {
+      const menuItems = document.querySelectorAll("#menubar li");
+
+      menuItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+          window.location.href = "https://pseudoteam.com/PseudoTeam2024/public/";
+        });
+      });
+    }
+  };
+</script>

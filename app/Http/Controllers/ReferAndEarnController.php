9c6ef33eb2709271c535d3dca0e0ade49439fb39
Sanskar shamra <sanskarsharma0119@gmail.com>
2025-08-07 14:27:03 +0530

    chnages made in sp referals

--- app/Http/Controllers/ReferAndEarnController.php
+++ app/Http/Controllers/ReferAndEarnController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Mail\ReferAndEarnMail;
 use App\Models\ProjectOwners;
 use App\Models\ReferralTable;
+use App\Models\ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -28,12 +29,12 @@ class ReferAndEarnController extends Controller
 
         $link = 'Pseudoteam.com/public/referal-registration/'.$customer_id.'/'.$userName;
 
-        ReferralTable::create([
-            'rfrls_user_id' => session('user_id'),
-            'rfrls_email_id' => $request->input('friend_email'),
-            'rfrls_promo_code' => $projectOwner->pown_username,
-            'rfrls_user_type'=> 'customer',
-        ]);
+        // ReferralTable::create([
+        //     'rfrls_user_id' => session('user_id'),
+        //     'rfrls_email_id' => $request->input('friend_email'),
+        //     'rfrls_promo_code' => $projectOwner->pown_username,
+        //     'rfrls_user_type'=> 'customer',
+        // ]);
 
         Mail::to($email)->send(new ReferAndEarnMail($link , $projectOwner->pown_name));
 
@@ -50,19 +51,18 @@ class ReferAndEarnController extends Controller
             return redirect()->back()->with('error', 'Customer id not found!');
         }
 
-        $projectOwner = ProjectOwners::where('pown_id' , $servicePartner)->first();
+        $servicePartner = ServiceProvider::where('sprov_id' , $servicePartner)->first();
 
         $email = $request->input('friend_email');
         $message = $request->input('message');
-        $userName = $projectOwner->pown_name;
+        $userName = $servicePartner->sprov_name;
 
 
-        ReferralTable::create([
-            'rfrls_user_id' => session('sp_user_id'),
-            'rfrls_email_id' => $request->input('frend_email'),
-            'rfrls_user_type'=> 'SP',
-        ]);
-
+        // ReferralTable::create([
+        //     'rfrls_user_id' => session('sp_user_id'),
+        //     'rfrls_email_id' => $request->input('frend_email'),
+        //     'rfrls_user_type'=> 'SP',
+        // ]);
 
         $link = 'Pseudoteam.com/referal-registration/'.$servicePartner.'/'.$userName;
 

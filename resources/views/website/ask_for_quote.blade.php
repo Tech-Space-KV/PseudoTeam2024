b5a8b707ee453eadd1c71dcfd4990d89ff3f877b
Sanskar shamra <sanskarsharma0119@gmail.com>
2025-08-16 19:05:29 +0530

    changes

--- resources/views/website/ask_for_quote.blade.php
+++ resources/views/website/ask_for_quote.blade.php
@@ -655,12 +655,8 @@
                     <div class="row g-3 mb-3">
                         <div class="col-md-6">
                             <label for="contact" class="form-label text-white">Contact</label>
-                            <!-- <input type="tel" class="form-control form-control-sm" name="contact" id="contact"
-                                required> -->
-                            <!-- <input type="tel" class="form-control form-control-sm" name="contact" id="contact" required
-                                pattern="[0-9]{10}" title="Please enter a 10-digit phone number"> -->
-                            <input type="password" class="form-control form-control-sm" name="contact" id="contact" required>
-
+                            <input type="text" class="form-control form-control-sm" name="contact" id="contact"
+                                required>
                         </div>
                         <div class="col-md-6">
                             <label for="company" class="form-label text-white">Company</label>
@@ -763,14 +759,14 @@
         if (searchQuery) {
             document.getElementById('tableSearch1').value = searchQuery;
 
-            searchTable1();
+            searchTable1(); 
         }
 
 
         if (searchQuery) {
             document.getElementById('tableSearch2').value = searchQuery;
 
-            searchTable2();
+            searchTable2(); 
         }
 
     </script>

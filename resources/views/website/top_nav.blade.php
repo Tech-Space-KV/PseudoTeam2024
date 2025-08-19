6001659550ef2d17b235fee6352f00e1bc43bb3d
Sanskar shamra <sanskarsharma0119@gmail.com>
2025-08-19 21:45:36 +0530

    Changes made by sanskar

--- resources/views/website/top_nav.blade.php
+++ resources/views/website/top_nav.blade.php
@@ -35,7 +35,7 @@
       </span>
 
       <span class="navbar-text">
-        <a class="btn btn-outline-light px-4 rounded-pill" onmouseover="this.style.color='black';"
+        <a class="btn px-4 rounded-pill border border-primary border-2 btn-outline-primary ms-2" onmouseover="this.style.color='black';"
           onmouseout="this.style.color='white';" href="{{ url('/authentication/customer/sign-in') }}">Manage Project</a>
       </span>
     </div>

620db0f2b003b227fb070882a84d059ae17cfd7d
Sanskar shamra <sanskarsharma0119@gmail.com>
2025-08-26 01:00:11 +0530

    Changes made by sanskar

--- app/Http/Controllers/ReferAndEarnController.php
+++ app/Http/Controllers/ReferAndEarnController.php
@@ -6,6 +6,7 @@ use App\Mail\ReferAndEarnMail;
 use App\Models\ProjectOwners;
 use App\Models\ReferralTable;
 use App\Models\ServiceProvider;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -30,16 +31,18 @@ class ReferAndEarnController extends Controller
         // $link = 'Pseudoteam.com/public/referal-registration/'.$customer_id.'/'.$userName;
         $link = 'Pseudoteam.com';
 
+        $promocode = Carbon::now()->format('YmdHis');
+
         ReferralTable::create([
             'rfrls_user_id' => session('user_id'),
             'rfrls_email_id' => $request->input('friend_email'),
-            'rfrls_promo_code' => $projectOwner->pown_username,
+            'rfrls_promo_code' => $promocode,
             'rfrls_user_type'=> 'customer',
         ]);
 
-        \Log::info('Working till here! ' . $projectOwner->pown_promo_code);
+        \Log::info('Working till here! ' . $promocode);
 
-        Mail::to($email)->send(new ReferAndEarnMail($link , $projectOwner->pown_name , $projectOwner->pown_promo_code));
+        Mail::to($email)->send(new ReferAndEarnMail($link , $projectOwner->pown_name , $promocode));
 
         return redirect()->back()->with('success', 'Invitation sent successfully!');
 
@@ -60,15 +63,18 @@ class ReferAndEarnController extends Controller
         $message = $request->input('message');
         $userName = $servicePartner->sprov_name;
 
+        $promocode = Carbon::now()->format('YmdHis');
+
         ReferralTable::create([
             'rfrls_user_id' => session('sp_user_id'),
             'rfrls_email_id' => $request->input('friend_email'),
+            'rfrls_promo_code' => $promocode,
             'rfrls_user_type'=> 'SP',
         ]);
 
         $link = 'Pseudoteam.com';
 
-        Mail::to($email)->send(new ReferAndEarnMail($link ,$servicePartner->sprov_name , $servicePartner->pown_promo_code));    
+        Mail::to($email)->send(new ReferAndEarnMail($link ,$servicePartner->sprov_name , $promocode));
 
         return redirect()->back()->with('success', 'Invitation sent successfully!');
 
